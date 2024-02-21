864927cb0cfc7ca3a8aaa7db7d396013181bb610
Pierre Lamey <pierre.lamey@epitech.eu>
2024-02-21 10:34:14 +0100

    [ADD] finished step 2

--- main.py
+++ main.py
@@ -13,20 +13,32 @@ def step_1(args):
 
 
 def step_2(args):
+    k = 1.00
     n = int(args[1])
-    k1 = float(args[2])
-    k2 = float(args[3])
-    for i in range():
-        pass
+    i0 = int(args[2])
+    i1 = int(args[3])
+    for i in range(301):
+        print("\n")
+        for j in range(i1 - i0 + 1):
+            populations = [n]
+            for i_bis in range(0, i1 + 1):
+                if i_bis != 0:
+                    populations.append(populations[i_bis - 1] * k * (1000 - populations[i_bis - 1]) / 1000)
+            result = populations[i0 + j]
+            print(f"{k:.2f} {result:.2f}")
+        k += 0.01
 
 
 def main(args):
     if len(args) == 3:
         step_1(args)
+        exit(0)
     elif len(args) == 4:
         step_2(args)
+        exit(0)
     else:
         print("USAGE: ./106bombyx n [k | i0 i1]")
+        exit(84)
 
 
 if __name__ == '__main__':
