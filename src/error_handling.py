ba33725a3e9cc3f18dd0956c64449d55e4969949
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-02-27 10:52:11 +0100

    Added error handling to keep k withing acceptable range

--- src/error_handling.py
+++ src/error_handling.py
@@ -3,5 +3,8 @@ PROJECT_NAME = "106bombyx"
 class NegativeNumberException(Exception):
     pass
 
+class KInvalidValue(Exception):
+    pass
+
 def display_error(error: str) -> None:
     print(PROJECT_NAME + ": " + error)
--- src/user_input.py
+++ src/user_input.py
@@ -18,10 +18,15 @@ def get_user_float(value: str) -> float:
         k = float(value)
         if (k < 0):
             raise NegativeNumberException
+        if (k > 4):
+            raise KInvalidValue
     except ValueError:
         display_error("Invalid argument \"{}\", please enter a valid floating point number".format(value))
         exit(84)
     except NegativeNumberException:
         display_error("Invalid argument \"{}\", please enter a positive value".format(value))
+        exit(84)
+    except KInvalidValue:
+        display_error("Invalid argument \"{}\", please enter a value below 4.0".format(value))
         exit(84)
     return k
