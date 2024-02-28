d05bb214127847639929a1e04b9dba790d4ff5ba
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-02-28 11:07:29 +0100

    Try at fixing floating point precision issues

--- src/maths.py
+++ src/maths.py
@@ -1,12 +1,13 @@
 from .user_input import *
+from decimal import Decimal
 from math import ceil
 
 def get_i0_num(n, k, i0):
-    populations = [n]
-    first_gen = 0
+    populations = [Decimal(n)]
+    first_gen = Decimal(0)
     for i in range(0, i0):
         if i != 0:
-            populations.append(populations[i - 1] * k * (1000 - populations[i - 1]) / 1000)
+            populations.append(Decimal(populations[i - 1]) * Decimal(k) * (Decimal(1000) - Decimal(populations[i - 1])) / Decimal(1000))
         first_gen = populations[i]
     return first_gen
 
@@ -23,8 +24,8 @@ def case1(args):
         print(f"{i + 1} {populations[i]:.2f}")
 
 def case2(args):
-    k = 1.00
-    n = get_user_int(args[1])
+    k = Decimal(1)
+    n = Decimal(get_user_int(args[1]))
     i0 = get_user_int(args[2])
     i1 = get_user_int(args[3])
 
@@ -39,6 +40,6 @@ def case2(args):
         populations = [i0_individuals_number]
         for i in range(i1 - (i0 - 1)):
             if i != i1:
-                populations.append(populations[i - 1] * k * (1000.0 - populations[i - 1]) / 1000.0)
-            print(f"{ceil(k * 100) / 100} {ceil(populations[i] * 100) / 100}")
-        k += 0.01
+                populations.append(Decimal(populations[i - 1]) * Decimal(k) * (Decimal(1000) - Decimal(populations[i - 1])) / Decimal(1000))
+            print(f"{k:.2f} {populations[i]:.2f}")
+        k += Decimal(0.01)
