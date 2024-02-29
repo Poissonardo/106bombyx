b42b53d0e1e990f5120d96635742ab97a6c52698
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-02-29 10:23:36 +0100

    Refactor maths.py to improve readability and fix potential division by zero bug

--- src/maths.py
+++ src/maths.py
@@ -1,30 +1,31 @@
 from .user_input import *
-from decimal import *
 from math import floor
 
 def get_i0_num(n, k, i0):
-    populations = [Decimal(n)]
-    first_gen = Decimal(0)
+    result = n
+    first_gen = 0
     for i in range(0, i0):
         if i != 0:
-            populations.append(Decimal(populations[i - 1]) * Decimal(k) * (Decimal(1000) - Decimal(populations[i - 1])) / Decimal(1000))
-        first_gen = populations[i]
+            result = result * k * ((1000 - result) / 1000)
+        first_gen = result
     return first_gen
 
 def case1(args):
     n = get_user_int(args[1])
     k = get_user_float(args[2])
-    populations = [n]
+    result = n
     if n > 1000:
         display_error("Invalid argument \"{}\", please enter a valid integer value".format(n))
         exit(84)
     for i in range(0, 100):
         if i != 0:
-            populations.append(populations[i - 1] * k * (1000 - populations[i - 1]) / 1000)
-        print(f"{i + 1} {populations[i]:.2f}")
+            result = result * k * (1000 - result) / 1000
+        if result < 0:
+            result = 0
+        print(f"{i + 1} {result:.2f}")
 
 def case2(args):
-    k = Decimal('1')
+    k = float(1.0)
     n = get_user_int(args[1])
     i0 = get_user_int(args[2])
     i1 = get_user_int(args[3])
@@ -35,11 +36,11 @@ def case2(args):
     if i0 > i1:
         display_error("Invalid argument \"{}\", i1 must be superior to i0".format(i1))
         exit(84)
-    while (k <= Decimal('4.0')):
+    while (k < 4.0):
         i0_individuals_number = get_i0_num(n, k, i0)
         result = i0_individuals_number
         for i in range(i1 - (i0 - 1)):
             if i != i1:
-                result = result * k * (Decimal('1000') - Decimal(result)) / Decimal('1000')
-            print(f"{k:.2f} {result:.2f}")
-        k = k + Decimal('0.01')
+                result = result * k * ((1000 - result) / 1000)
+            print(f"{k:.2f} {round(result, 2):.2f}")
+        k += 0.01
