398c053f598e173192ea45d8061390cf3fa20d15
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-02-29 11:25:19 +0100

    Fix floating point precision in maths.py

--- src/maths.py
+++ src/maths.py
@@ -25,7 +25,7 @@ def case1(args):
         print(f"{i + 1} {result:.2f}")
 
 def case2(args):
-    k = float(1.0)
+    k = float(1)
     n = get_user_int(args[1])
     i0 = get_user_int(args[2])
     i1 = get_user_int(args[3])
@@ -36,11 +36,18 @@ def case2(args):
     if i0 > i1:
         display_error("Invalid argument \"{}\", i1 must be superior to i0".format(i1))
         exit(84)
-    while (k < 4.0):
-        i0_individuals_number = get_i0_num(n, k, i0)
-        result = i0_individuals_number
-        for i in range(i1 - (i0 - 1)):
-            if i != i1:
-                result = result * k * ((1000 - result) / 1000)
+    while k < 4:
+        result = float(n)
+        j = 1
+        while j < i0:
+            result = (k * result * ((1000 - result) / 1000))
+            if (result < 0):
+                result = 0
+            j += 1
+        while (j <= i1):
             print(f"{k:.2f} {round(result, 2):.2f}")
+            result = (k * result * ((1000 - result) / 1000))
+            if (result < 0):
+                result = 0
+            j += 1
         k += 0.01
